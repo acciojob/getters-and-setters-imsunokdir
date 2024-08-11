397907bd80c6967392ea7a598d80c87b5da185da
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-08-11 10:47:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age
+	}
+	get name(){
+		return this._name;
+	}
+	set age(newAge){
+		this._age = newAge
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	constructor(name, age){
+		super(name, age)
+	}
+	study(){
+		console.log(this.name+" is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	constructor(name, age){
+		super(name, age)
+	}
+	teach(){
+		console.log(this.name+" is teaching")
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
